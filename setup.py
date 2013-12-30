e6c4d1e12f103245756b4df6bf37e07b4cb8f5e0
Wil Gieseler <wil@wilgieseler.com>
2013-12-30 02:52:05 -0800

    lets go old school

--- setup.py
+++ setup.py
@@ -1,6 +1,13 @@
 from subprocess import call
 import urllib
 
+print("-----> [minecloud-mapper/setup.py] Installing dependencies with pip")
+
+call(["pip", "install", "numpy==1.8.0"])
+call(["pip", "install", "Pillow==2.2.2"])
+call(["pip", "install", "wsgiref==0.1.2"])
+call(["pip", "install", "boto==2.21.0"])
+call(["pip", "install", "boto-rsync==0.8.1"])
 
 print("-----> [minecloud-mapper/setup.py] Cloning overviewer")
 call(["git", "clone", "--depth=1", "https://github.com/overviewer/Minecraft-Overviewer.git", "overviewer"])
